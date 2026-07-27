4f66b8e80d9e3bb4d2a70c7a65644ed83e30c6b9
Rumi <legendrumi11@gmail.com>
2026-07-27 19:27:06 +0500

    1.0.2

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -8,7 +8,7 @@
             <div class="position-relative mb-3">
                 <img src="{{ asset('img/book1.jpg') }}" class="w-30 rounded-3" alt="{{ $book->name }}" style="height: 180px; object-fit: cover;">
             </div>
-  {{--ss  --}}
+  {{-- sjf  --}}
             <div>
                 <h2 class="text-success mb-3 fw-bold">{{ $book->name }}</h2>
                 <hr>
